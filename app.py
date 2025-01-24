00dc722e0a940d27ed9d15ba80328ac2175f5f91
naza1144 <85209746+naza1144@users.noreply.github.com>
2025-01-24 14:03:31 +0700

    สร้างคลาสแรกบน github repo 555

---
+++ app.py
@@ -0,0 +1,12 @@
+class Student(object):
+    Student_count = 0 
+    
+    def __new__(self):
+        print('Student.__new__')
+
+    def __init__(self):
+        print('Student.__init__')
+        self.gpa = 4.00
+
+    def student_hard(self):
+        print('Sir, yes sir.')
